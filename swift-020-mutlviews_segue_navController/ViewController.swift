3cac7a9eae455a9f5b69776400ff2be8a7fc7a58
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-08-22 08:18:41 -0300

    Initial Commit

---
+++ swift-020-mutlviews_segue_navController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-020-mutlviews_segue_navController
+//
+//  Created by Luiz Carlos da Silva Araujo on 22/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
